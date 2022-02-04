8819c32bf5af022c7df36e00d44468e4413f21df
mark-akbarov <mark-akbarov@gmail.com>
2022-02-04 15:27:51 +0500

    Add styles

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'ckeditor',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
