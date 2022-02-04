d6f12c9791020f871c77dfba16c4549d90d1fda4
mark-akbarov <mark-akbarov@gmail.com>
2022-02-04 22:32:32 +0500

    Add Top Posts feature

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView,  BlogUpdateView, BlogDeleteView
+from .views import (BlogListView, BlogDetailView, 
+BlogCreateView,  BlogUpdateView, BlogDeleteView, TopBlogListView)
+from . import views
 
 
 urlpatterns = [
    path('', BlogListView.as_view(), name='home'),
-   path('top_posts/', BlogListView.as_view(), name='top_posts'),
+   path('top_posts/', TopBlogListView.as_view(), name='top_posts'),
    path('blog/new/', BlogCreateView.as_view(), name='blog_create'),
    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
    path('blog/<int:pk>/update/', BlogUpdateView.as_view(), name='blog_update'),
