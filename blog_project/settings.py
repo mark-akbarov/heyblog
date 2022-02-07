4349651a77189ba0f689c60423e3bb439e92b3e3
mark-akbarov <mark-akbarov@gmail.com>
2022-02-07 20:28:13 +0500

    Add styles

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -149,3 +149,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'markakbarov@gmail.com'
+EMAIL_HOST_PASSWORD = 'm4rcakbAr0v'
+EMAIL_PORT = 587
--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
-from re import template
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.views.generic import DetailView
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 from .models import Profile
 
@@ -32,6 +39,34 @@ def register(request):
             return redirect('login')
 
     return render(request, 'users/register.html', ctx)
+
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.is_active = False
+#             user.save()
+#             current_site = get_current_site(request)
+#             mail_subject = 'Activate your blog account.'
+#             message = render_to_string('users/activate_email.html', {
+#                 'user': user,
+#                 'domain': current_site.domain,
+#                 'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+#                 'token':account_activation_token.make_token(user),
+#             })
+#             to_email = form.cleaned_data.get('email')
+#             email = EmailMessage(
+#                         mail_subject, message, to=[to_email]
+#             )
+#             email.send()
+#             return HttpResponse('Please confirm your email address to complete the registration')
+#     else:
+#         form = RegisterForm()
+#     return render(request, 'users/register.html', {'form': form})
+
 
 
 class ProfileDetailView(DetailView):
