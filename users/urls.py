5b0e99ded68bb8f7098ca9bf2fc1ff8ca4b2fab4
mark-akbarov <mark-akbarov@gmail.com>
2022-02-10 20:21:34 +0500

    Add styles to Profile page

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
--- users/views.py
+++ users/views.py
@@ -17,56 +17,49 @@ from .models import Profile
 
 
 
-# def register(request):
-#     form = RegisterForm(request.POST or None)
-#     context = {'form': form}
-#     if request.method == "POST":
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             name = form.cleaned_data['name']
-#             email = form.cleaned_data['email']
-#             password_one = form.cleaned_data['password_one']
-#             password_two = form.cleaned_data['password_two']
-            
-#             return redirect('login')
-
-#     return render(request, 'users/register.html', context)
-
-
-
 def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
+    form = SignupForm(request.POST or None)
+    context = {'form': form}
+    if request.method == "POST":
         if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-            current_site = get_current_site(request)
-            mail_subject = 'Activate your blog account.'
-            message = render_to_string('users/activate_email.html', {'user': user, 'domain': current_site.domain, 'uid':urlsafe_base64_encode(force_bytes(user.pk)), 'token': account_activation_token.make_token(user)})
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(mail_subject, message, to=[to_email])
-            email.send()
-            return HttpResponse('Please confirm your email address to complete the registration')
-    else:
-        form = SignupForm()
-    return render(request, 'users/signup.html', {'form': form})
+            username = form.cleaned_data['username']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password_one = form.cleaned_data['password_one']
+            password_two = form.cleaned_data['password_two']
+            
+            return redirect('login')
+
+    return render(request, 'users/signup.html', context)
 
 
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
-    else:
-        return HttpResponse('Activation link is invalid!')
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.is_active = False
+#             user.save()
+#     else:
+#         form = SignupForm()
+#     return render(request, 'users/signup.html', {'form': form})
+
+
+# def activate(request, uidb64, token):
+#     try:
+#         uid = force_text(urlsafe_base64_decode(uidb64))
+#         user = User.objects.get(pk=uid)
+#     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+#         user = None
+#     if user is not None and account_activation_token.check_token(user, token):
+#         user.is_active = True
+#         user.save()
+#         login(request, user)
+#         # return redirect('home')
+#         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+#     else:
+#         return HttpResponse('Activation link is invalid!')
 
 
 class ProfileDetailView(DetailView):
