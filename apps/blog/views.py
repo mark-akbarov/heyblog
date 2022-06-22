83b88a6526398b19792b47b0dde1e23cfba11681
Umarkhon Akbarov <mark-akbarov@gmail.com>
2022-06-22 12:35:35 +0500

    Delete Comment

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -7,8 +7,7 @@ from django.contrib.auth.mixins import (
 LoginRequiredMixin, 
 )
 from django.db.models import Q
-from .models import Blog, BlogComment
-from .forms import NewCommentForm
+from .models import Blog
 
 
 def home(request):
@@ -38,24 +37,7 @@ class BlogDetailView(DetailView):
     model = Blog
     template_name = 'blog/blog_detail.html'
     login_url = 'login'
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-
-        comments_connected = BlogComment.objects.filter(
-            blogpost_connected=self.get_object()).order_by('-date_posted')
-        data['comments'] = comments_connected
-        if self.request.user.is_authenticated:
-            data['comment_form'] = NewCommentForm(instance=self.request.user)
-
-        return data
-
-    def post(self, request, *args, **kwargs):
-        new_comment = BlogComment(content=request.POST.get('content'),author=self.request.user,blogpost_connected=self.get_object(), )
-        new_comment.save()
-        return self.get(self, request, *args, **kwargs)
-        
-
+    
 class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Blog 
     fields = ['title', 'text', 'image', 'tags', 'status']
