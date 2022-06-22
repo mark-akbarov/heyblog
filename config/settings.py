bcfe06e9c94a92723cb51068f52105d85c9fb064
Umarkhon Akbarov <mark-akbarov@gmail.com>
2022-06-22 12:01:11 +0500

    q

--- config/settings.py
+++ config/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reddit_db',
+        'USER': 'reddit_user',
+        'PASSWORD': '2404',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
